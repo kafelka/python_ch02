94330f0e6389cddf92bb307096f4651275f2f470
Mag Kafel <na.hasiok@gmail.com>
2018-11-29 16:35:20 +0000

    LPTHW exercises 1-10, first 5 completed

---
+++ ex1-10.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 12:17:23 2018
+
+@author: 612383300
+"""
+
+#print("Hello world!")
+#print("Hello again!")
+#print("I like typing this")
+#print("This is done")
+#print("Yay! Printing")
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+
+
+##ex3
+#
+#print ("I will now count my chickens:")
+#
+#print ("Hens", 25 + 30 / 6)
+#
+#print ("Roosters", 100 - 25 * 3 % 4)
+#
+#print ("Now I will count the eggs:")
+#
+#print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#
+#print ("Is it true that 3 + 2 < 5 - 7?")
+#
+#print (3 + 2 < 5 - 7)
+#
+#print ("What is 3 + 2?", 3 + 2)
+#
+#print ("What is 5 - 7?", 5 - 7)
+#
+#print ("Oh, that's why it's False.")
+#
+#print ("How about some more.")
+#
+#print ("Is it greater?", 5 > -2)
+#
+#print ("Is it greater or equal?", 5 >= -2)
+#
+#print ("Is it less or equal?", 5 <= -2)
+
+
+##ex4
+#
+#cars = 100
+#space_in_a_car = 4
+#drivers = 30
+#passengers = 90
+#cars_not_driven = cars - drivers
+#cars_driven = drivers
+#carpool_capacity = cars_driven * space_in_a_car
+#average_passengers_per_car = passengers / cars_driven
+#
+#
+#print ("There are", cars, "cars available.")
+#print ("There are only", drivers, "drivers available.")
+#print ("There will be", cars_not_driven, "empty cars today.")
+#print ("We can transport", carpool_capacity, "people today.")
+#print ("We have", passengers, "to carpool today.")
+#print ("We need to put about", average_passengers_per_car, "in each car.")
+
+#ex5
+
+my_name = 'Maggie'
+my_age = 100 
+my_height = 165 # cm
+my_weight = 53 # kg
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Dark brown'
+
+print ("Let's talk about {}".format(my_name))
+print ("He's {} cm tall.".format(my_height))
+print ("He's {} kg heavy.".format(my_weight))
+print ("Actually that's not too heavy.")
+print ("She's got {} eyes and {} hair.".format(my_eyes, my_hair))
+print ("Her teeth are usually {} depending on the coffee.".format(my_teeth))
+
+# this line is tricky, try to get it exactly right
+print ("If I add {}, {}, and {} I get {}.".format(
+    my_age, my_height, my_weight, my_age + my_height + my_weight))
+
+
+#ex6
+
